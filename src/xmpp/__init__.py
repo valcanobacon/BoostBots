034c53f3baa0a196f64812f0cd02b2c1e0084693
Alecks Gates <agates@mail.agates.io>
2023-07-02 16:13:19 -0500

    Pars XMPP JID from podcast:chat

--- src/xmpp/__init__.py
+++ src/xmpp/__init__.py
@@ -3,13 +3,16 @@ import datetime
 import functools
 import json
 import logging
+from typing import Optional
 
 import aioxmpp
 import click
+import requests
+from bs4 import BeautifulSoup
 from lndgrpc import AsyncLNDClient
-from lndgrpc.aio.async_client import ln
 
 from ..numerology import number_to_numerology
+from ..podcast_index import PodcastIndex
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -31,6 +34,8 @@ def async_cmd(func):
 @click.option("--xmpp-account-password")
 @click.option("--xmpp-room-jid")
 @click.option("--xmpp-nick", default="BoostBot XMPP")
+@click.option("--podcast-index-api-key")
+@click.option("--podcast-index-api-secret")
 @click.option("--minimum-donation", type=int)
 @click.option("--allowed-name", multiple=True)
 @click.pass_context
@@ -45,10 +50,18 @@ async def cli(
     xmpp_account_password,
     xmpp_room_jid,
     xmpp_nick,
+    podcast_index_api_key,
+    podcast_index_api_secret,
     minimum_donation,
     allowed_name,
 ):
     ctx.ensure_object(dict)
+
+    podcast_index = PodcastIndex(
+        user_agent="BoostBots",
+        api_key=podcast_index_api_key,
+        api_secret=podcast_index_api_secret,
+    )
 
     client = aioxmpp.Client(
         aioxmpp.JID.fromstr(xmpp_account_jid),
@@ -97,7 +110,7 @@ async def cli(
                         logging.debug("Donation too low, skipping: %s", data)
                         continue
 
-                    await send_to_xmpp(muc_client, xmpp_room_jid, xmpp_nick, data, value)
+                    await send_to_xmpp(muc_client, xmpp_room_jid, xmpp_nick, podcast_index, data, value)
 
                 except aioxmpp.errors.XMPPError as error:
                     logging.exception("error: %s", error)
@@ -107,16 +120,49 @@ async def send_to_xmpp(
     muc_client: aioxmpp.muc.MUCClient,
     xmpp_room_jid: str,
     xmpp_nick: str,
+    podcast_index: PodcastIndex,
     data,
     value
 ):
-    # TODO: Get room jid from RSS feed <podcast:chat> element
-    # This is different from IRC because you can join any room on any server
-    #   (as long as the given account isn't banned and has access)
+    feed_url = data.get("url")
+    if not feed_url:
+        feed_url = get_feed_url_from_podcast_index(podcast_index, data)
+        if not feed_url:
+            return
+
+    logging.debug(feed_url)
+
+    soup = get_feed(feed_url)
+    if not soup:
+        return
+
+    items = soup.find_all(["podcast:liveItem", "item"])
+    item = next(
+        filter(
+            lambda x: x.title.get_text() == data.get("episode")
+                      or x.guid.get_text() == data.get("guid"),
+            items,
+        ),
+        None,
+    )
+    if not item:
+        return
+
+    logging.debug(item)
+
+    chat = item.find("podcast:chat", protocol="xmpp")
+    if not (chat and chat.get("space")):
+        return
+
+    logging.debug(chat)
+
+    logging.info(f"Joining room '{chat['space']}'")
+
     room, join_future = muc_client.join(
-        aioxmpp.JID.fromstr(xmpp_room_jid),
+        aioxmpp.JID.fromstr(chat["space"]),
         xmpp_nick,
-        history=None
+        # Don't bother to fetch room history when joining
+        history=aioxmpp.muc.xso.History(maxchars=0, maxstanzas=0, seconds=0)
     )
 
     message = muc_message(data, value)
@@ -153,3 +199,23 @@ def muc_message(data, value):
     xmpp_message.body[None] = message
 
     return xmpp_message
+
+
+def get_feed(feed_url) -> Optional[BeautifulSoup]:
+    response = requests.get(feed_url)
+    if response.status_code not in [requests.status_codes.codes.ok]:
+        return
+
+    return BeautifulSoup(response.text, features="xml")
+
+
+def get_feed_url_from_podcast_index(podcast_index: PodcastIndex, data) -> Optional[str]:
+    feed_id = data.get("feedID")
+    if not feed_id:
+        return
+
+    try:
+        result = podcast_index.podcasts_byfeedid(feed_id)
+        return result.data["feed"]["url"]
+    except:
+        pass
